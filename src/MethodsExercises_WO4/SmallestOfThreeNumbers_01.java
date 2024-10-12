079c543125000c61da07c14a0c141029fe4eb1d7
beloslavageorgieva <beloslava.mg@gmail.com>
2024-10-12 21:43:15 +0300

    tasks

---
+++ src/MethodsExercises_WO4/SmallestOfThreeNumbers_01.java
@@ -0,0 +1,22 @@
+package MethodsExercises_WO4;
+
+import java.util.Scanner;
+
+public class SmallestOfThreeNumbers_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int firstNumber = Integer.parseInt(scanner.nextLine());
+        int secondNumber = Integer.parseInt(scanner.nextLine());
+        int thirdNumber = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(getSmallestNumber(firstNumber, secondNumber, thirdNumber));
+    }
+
+    //метод, който връща най-малкото от три числа
+    //при създаване на метод: {public / private} static {void / } {име на метода} ({параметри})
+    private static int getSmallestNumber (int n1, int n2, int n3) {
+        return Math.min(Math.min(n1, n2), n3);
+
+    }
+}
---
+++ src/MethodsExercises_WO4/VowelsCount_02.java
@@ -0,0 +1,33 @@
+package MethodsExercises_WO4;
+
+import java.util.Scanner;
+
+public class VowelsCount_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //входни данни
+        String text = scanner.nextLine().toLowerCase();
+        //"bELoslava".toLowerCase() -> "beloslava"
+
+        printVowelsCount(text);
+
+    }
+
+    //метод, който отпечатва броя на гласните букви в текста
+    public static void printVowelsCount (String text) {
+        //малки гласни букви: a, o, u, e, i
+        int count = 0; //брой на гласните букви
+
+        //text = "softuni".toCharArray() -> ['s', 'o', 'f', 't', 'u', 'n', 'i']
+        for (char symbol : text.toCharArray()) {
+            //проверка дали е малка гласна буква
+            if (symbol == 'a' || symbol == 'o' || symbol == 'u'
+                    || symbol == 'e' || symbol == 'i') {
+                count++;
+            }
+        }
+
+        //знаем броят на малките гласни букви
+        System.out.println(count);
+    }
+}
